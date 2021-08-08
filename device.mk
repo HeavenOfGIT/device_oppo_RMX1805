9164335e782b94a172f2c10fd815218f294b0da3
Abhinav Verma <77457859+abhinavftp98@users.noreply.github.com>
2021-08-08 16:17:55 +0530

    RMX1805: Add OPgallery

--- device.mk
+++ device.mk
@@ -57,6 +57,10 @@ PRODUCT_PACKAGES += \
     RealmeParts \
     RealmeDirac
 
+#OPgallery
+PRODUCT_PACKAGES += \
+   OPgallery
+
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
